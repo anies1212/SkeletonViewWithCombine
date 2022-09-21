54de927179bf587c08dd94a1c4b1e366f9cb704e
arakaki_seina <87635517+anies1212@users.noreply.github.com>
2022-09-21 20:42:50 +0900

    Initial Commit

---
+++ CombineSkeletonView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CombineSkeletonView
+//
+//  Created by 新垣 清奈 on 2022/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
